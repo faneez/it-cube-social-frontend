dc3ff22326f608438cf44dafc596f5be8589d07e
faneez <yuzhakov8@mail.ru>
2023-04-11 06:21:07 +0500

    trying to add ngrok

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const instance = axios.create({
-	baseURL: "https://it-cube-social-backend-production.up.railway.app",
+	baseURL: "https://7026-94-51-38-102.ngrok-free.app",
+	// "https://it-cube-social-backend-production.up.railway.app",
 })
 
 export default instance
